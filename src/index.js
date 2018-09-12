02a929426fad09915e1f4b18ff4ba9839d4d23ee
VladikRebko <rebko.v.v@mail.com>
2018-09-12 23:02:46 +0300

    quadratic-equation

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+	
+	let arrayOfSplit = equation.split(' ');
+	let a = +arrayOfSplit[0];
+	let b;
+	let c;
+	
+
+	( arrayOfSplit[3] === "-") &&  ( b = +(arrayOfSplit[3] + arrayOfSplit[4]) );
+	( arrayOfSplit[3] != "-") &&	( b = +arrayOfSplit[4] );
+
+	( arrayOfSplit[7] === "-") &&  ( c = +(arrayOfSplit[7] + arrayOfSplit[8]) );
+	( arrayOfSplit[7] != "-") &&	( c = +arrayOfSplit[8] );
+	
+	let disc;
+	let x1;
+	let x2;
+
+	disc = b * b - (4 * a * c);
+
+	x1 = ((-1) * b + Math.sqrt(disc))/( 2 * a );
+	x2 = ((-1) * b - Math.sqrt(disc))/( 2 * a );
+
+	x1 = Math.round(x1);
+	x2 = Math.round(x2);
+
+	let solution;
+
+	if(x1 < x2)
+	 solution = [x1, x2];
+	else solution = [x2, x1];
+
+	return solution;
+	
 }
+
